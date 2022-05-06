d3d1388b862d21e5fcaa941686c6c0ecd8e14659
renjie-run <rj.aiyayao@gmail.com>
2022-05-06 23:29:36 +0800

    user: register

--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -7,9 +7,13 @@ type AnyFunc<T = any> = (...args: any[]) => T;
 type CanExportFunc = AnyFunc<Promise<any>> | AnyFunc<IterableIterator<any>>;
 type AutoInstanceType<T, U = T extends CanExportFunc ? T : T extends AnyFunc ? ReturnType<T> : T> = U extends AnyClass ? InstanceType<U> : U;
 import ExportTest from '../../../app/service/Test';
+import ExportBase from '../../../app/service/base';
+import ExportUser from '../../../app/service/user';
 
 declare module 'egg' {
   interface IService {
     test: AutoInstanceType<typeof ExportTest>;
+    base: AutoInstanceType<typeof ExportBase>;
+    user: AutoInstanceType<typeof ExportUser>;
   }
 }
