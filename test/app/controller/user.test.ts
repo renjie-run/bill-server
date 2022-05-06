9a269c5d85425943120b6e8d9468b7436fc82b63
renjie-run <rj.aiyayao@gmail.com>
2022-05-07 07:23:48 +0800

    remove tests

--- test/app/controller/user.test.ts
+++
@@ -1,8 +0,0 @@
-// import * as assert from 'assert';
-import { app } from 'egg-mock/bootstrap';
-
-describe('test/app/controller/user.test.ts', () => {
-  it('should POST /api/user', async () => {
-    await app.httpRequest().post('/api/user').expect(400);
-  });
-});
